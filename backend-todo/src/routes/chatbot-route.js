cddd9d40f7644c5fb72b533317ebda11000c97f2
shwetank-nitp <sweetankr.ug23.ee@nitp.ac.in>
2025-08-02 15:15:52 +0530

    renamed

--- backend-todo/src/routes/chatbot-route.js
+++
@@ -1,9 +0,0 @@
-const { Router } = require("express");
-const verifyToken = require("../middlewares/decode-token");
-const { chatbotMessagesProcessing } = require("../controllers/llm-controller");
-
-const chatBotRouter = Router();
-
-chatBotRouter.post("/chat", verifyToken, chatbotMessagesProcessing);
-
-module.exports = { chatBotRouter };
